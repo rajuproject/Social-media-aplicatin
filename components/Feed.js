cf6d346e67496c438070c5091b98963471dd5d82
khairul <md.raju130@yahoo.com>
2023-02-22 04:33:44 +0600

    home added

---
+++ components/Feed.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { SparklesIcon } from '@heroicons/react/24/solid'
+function Feed() {
+  return (
+    <div className='xl:ml-[370px] border-l border-r xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl' >
+    
+    <div className='flex justify-between py-3 px-3 sticky top-0 border-b z-50 bg-white '>
+    <h2 className='text-lg sm:text-xl font-bold cursor-pointer'>Home</h2>
+    <div className='hoverEffect'>
+<SparklesIcon className='h-5'/>
+    </div>
+
+    </div>
+    
+    </div>
+  )
+}
+
+export default Feed
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import Sidebar from '@/components/Sidebar'
+import Feed from "../components/Feed"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,6 +23,8 @@ export default function Home() {
       <Sidebar/>
 
       {/* Feed  */}
+
+      <Feed/>
 
       {/* Widgets  */}
 
